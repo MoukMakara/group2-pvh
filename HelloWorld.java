b0b073d637a09bb27d5d390465f74d0ba1aab0d7
Dita rector <ditarector@ToTo.local>
2026-02-11 15:37:49 +0700

    push a new Hello world,java

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+abstract class Animal{
+    abstract void makeSound();
+
+    public void eat() {
+        System.out.println("This animal eats food.");
+    }
+
+}
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        
+    }
+
+    
+}
